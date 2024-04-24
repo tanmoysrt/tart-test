f9216f49652dbba09cd5d7223347ae9698d81381
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-04-24 20:02:58 +0530

    feat: stats added to debug

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import { generateTarBlob } from "./tarts";
 function App() {
 
   const handleClick = async () => {
+    const startTime = Date.now();
+    document.getElementById("status").innerText = "Generating tar file...";
     const tarBlob = await generateTarBlob(document.getElementById("file-input").files);
-    console.log(tarBlob)
+    const requiredTime = Date.now() - startTime;
     document.getElementById("size").innerText = (tarBlob.size / 1024 / 1024).toFixed(2) + " MB";
     document.getElementById("download-link").href = URL.createObjectURL(tarBlob);
+    document.getElementById("time").innerText = (requiredTime / 1000) + " seconds";
+    document.getElementById("status").innerText = "Done";
   }
 
   return (
     <div style={{ "padding": "40px" }}>
+      <p>Status : &nbsp;<span id="status"></span></p>
+      <p>Time : &nbsp;<span id="time"></span></p>
       <p>Size : &nbsp;<span id="size"></span></p>
       <p>Tar file : &nbsp;<a href="#" download="test.tar" id="download-link">Download</a></p>
       <input
